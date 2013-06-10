32f804379ac517dbb8455b2bbe6fa954a73fbbb3
Greg Madison <greg.madison83@gmail.com>
2013-06-10 16:11:53 -0400

    corrected path in config.sh

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ ! -d /home/$USER/.vim ]; then
-	ln -s "/home/$USER/.config/vim" "/home/$USER/.vim"
+	ln -s "/home/$USER/.config_files/vim" "/home/$USER/.vim"
 fi
 
 if [ ! -d /home/$USER/.vimrc ]; then
-	ln -s "/home/$USER/.config/vimrc" "/home/$USER/.vimrc"
+	ln -s "/home/$USER/.config_files/vimrc" "/home/$USER/.vimrc"
 fi
 
 if [ ! -d /home/$USER/.gitconfig ]; then
-	ln -s "/home/$USER/.config/gitconfig" "/home/$USER/.gitconfig"
+	ln -s "/home/$USER/.config_files/gitconfig" "/home/$USER/.gitconfig"
 fi
 
-#TODO Research/create arrays of all folders to then have a function itterate through them, creating all links.
+#TODO Research/create arrays of all folders to then have a function itterate through them, creating all links. In other words, stop being lazy.
