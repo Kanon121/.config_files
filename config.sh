d5aa7cf875d40a04cbfe18b428f439c134e7e17c
Greg Madison <greg.madison83@gmail.com>
2014-06-30 14:11:39 -0400

    added bashrc to config script

--- config.sh
+++ config.sh
@@ -12,4 +12,9 @@ if [ ! -d /home/$USER/.gitconfig ]; then
 	ln -s "/home/$USER/.config_files/gitconfig" "/home/$USER/.gitconfig"
 fi
 
+if [ ! -d /home/$USER/.bashrc ]; then
+	mv "/home/$USER/.bashrc" "/home/$USER/.bashrc.OLD"
+	ln -s "/home/$USER/.config_files/bashrc" "/home/$USER/.bashrc"
+fi
+
 #TODO Research/create arrays of all folders to then have a function itterate through them, creating all links. In other words, stop being lazy.
